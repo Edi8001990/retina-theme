ef5a10be82d49e860b81d54b3fe29dc9155fe8cc
Mateusz Trajdos <mateusz@thehubpeople.com>
2021-12-06 17:03:55 +0100

    Advancded custom Fields for URL CTA's initialized...

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
     <div class="container">
 
         
-
+        <?php get_template_part('includes/section'  ,'head') ?>
         <?php get_template_part('includes/section'  ,'content') ?>
         <?php get_template_part('includes/section'  ,'contact') ?>
     </div>
--- includes/section-head.php
+++ includes/section-head.php
@@ -1,1 +1,20 @@
-This is section head.
+<?php 
+$post   = get_page_by_title( 'Hero Header Title' );
+$output =  apply_filters( 'the_content', $post->post_content );
+$page_title = apply_filters( 'the_title', $post->post_title );
+
+echo $page_title;
+echo $output;
+?>
+
+
+<!-- Customizable top section CTA's -->
+
+<?php if( get_field('choose_industry_filter') ): ?>
+	<a href="<?php the_field('choose_industry_filter'); ?>">Choose Industry</a>
+<?php endif; ?>
+
+
+<?php if( get_field('choose_degree_filter') ): ?>
+	<a href="<?php the_field('choose_degree_filter'); ?>">Choose Degree</a>
+<?php endif; ?>
