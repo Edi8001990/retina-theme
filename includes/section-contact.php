f264f59300d1de3734bbe3893bb3ef9817ee61a6
Mateusz Trajdos <mateusz@thehubpeople.com>
2021-12-08 18:27:35 +0100

    Contact us Styling completed

--- includes/section-contact.php
+++ includes/section-contact.php
@@ -1,5 +1,12 @@
 <?php 
 $post   = get_page_by_title( 'Contact Us' );
 $output =  apply_filters( 'the_content', $post->post_content );
-echo $output;
 ?>
+
+
+<section>
+    <div class="contact-section">
+        <h1><?php the_field('contact_us_header'); ?></h1>
+        <span class="contact-paragraph"><?php echo $output; ?></span>
+    </div>
+</div>
--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+// Add .active CSS class to features to make it hidden or visible 
+
 jQuery(document).ready(function(){
     jQuery(".plus-toggle").click(function(){
             jQuery(this)
@@ -12,4 +14,10 @@ jQuery(document).ready(function(){
             .find(".plus-toggle, .show-text, .close-toggle, .features-image-wrapper, .feature-description, .col-green, .feature-page-url, .feature-header")
             .removeClass('active');
     });
+});
+
+// Add boostrap class to select input in the form
+jQuery(document).ready(function(){
+    jQuery( ".country-select .wpcf7-form-control-wrap select" ).addClass("form-select"); 
+    jQuery( ".education-level .wpcf7-form-control-wrap select" ).addClass("form-select"); 
 });
